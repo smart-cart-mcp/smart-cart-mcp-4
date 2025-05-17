f887e8143c98792c75d7088a34b68cf62464dd78
Jacob Valdez <jacobfv123@gmail.com>
2025-05-17 16:56:17 -0700

    save

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
+export const dynamic = 'force-dynamic';
+
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
